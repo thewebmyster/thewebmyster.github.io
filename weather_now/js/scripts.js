34826a34c39ad569074d5350f8ba47dfe79437d2
Paul Cheney <pwcbyui.edu>
2019-04-02 18:12:10 -0600

    protocol

--- weather_now/js/scripts.js
+++ weather_now/js/scripts.js
@@ -1,5 +1,5 @@
 const weatherObject = new XMLHttpRequest();
-weatherObject.open("GET","http://api.openweathermap.org/data/2.5/weather?zip=83127,us&appid=437209d363a4387bb2f5dffcea58bbee", true);
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=83127,us&appid=437209d363a4387bb2f5dffcea58bbee", true);
 weatherObject.send();
 weatherObject.onload = function() {
     let weatherInfo = JSON.parse(weatherObject.responseText);
