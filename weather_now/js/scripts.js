e40c3794b59c666d81db78f11737a91f913c93a6
Paul Cheney <pwcbyui.edu>
2019-04-06 17:24:06 -0600

    update to Boston

--- weather_now/js/scripts.js
+++ weather_now/js/scripts.js
@@ -1,5 +1,5 @@
 const weatherObject = new XMLHttpRequest();
-weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=84653,us&appid=437209d363a4387bb2f5dffcea58bbee&units=imperial", true );
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=02111,us&appid=437209d363a4387bb2f5dffcea58bbee&units=imperial", true );
 weatherObject.send();
 weatherObject.onload = function() {
     let weatherInfo = JSON.parse(weatherObject.responseText);
@@ -13,5 +13,5 @@ weatherObject.onload = function() {
     const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
     console.log(icon_path);
     document.getElementById('weather_icon').src = icon_path;
-    
+
 } // end of onload
