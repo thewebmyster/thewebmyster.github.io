d64ab47ba9ebc980882530855e0ca57e6f7c112f
Paul Cheney <pwcbyui.edu>
2019-04-06 09:37:48 -0600

    units changed

--- weather_now/js/scripts.js
+++ weather_now/js/scripts.js
@@ -1,5 +1,5 @@
 const weatherObject = new XMLHttpRequest();
-weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=84653,us&appid=437209d363a4387bb2f5dffcea58bbee@units=imperial", true );
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=84653,us&appid=437209d363a4387bb2f5dffcea58bbee&units=imperial", true );
 weatherObject.send();
 weatherObject.onload = function() {
     let weatherInfo = JSON.parse(weatherObject.responseText);
