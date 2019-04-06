b0ad6a57537ad620d8ea33897bd6ac388a36bb14
Paul Cheney <pwcbyui.edu>
2019-04-06 17:13:08 -0600

    added path

--- weather_now/js/scripts.js
+++ weather_now/js/scripts.js
@@ -10,6 +10,7 @@ weatherObject.onload = function() {
     document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
 
     const iconcode = weatherInfo.weather[0].icon;
-    console.log(iconcode);
-    
+    const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+    console.log(icon_path);
+
 } // end of onload
