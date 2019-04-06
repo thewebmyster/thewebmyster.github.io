be88e95ed3693c1058c7502e75084ab783b5ee7c
Paul Cheney <pwcbyui.edu>
2019-04-06 17:22:41 -0600

    update to weather ison

--- weather_now/js/scripts.js
+++ weather_now/js/scripts.js
@@ -12,5 +12,6 @@ weatherObject.onload = function() {
     const iconcode = weatherInfo.weather[0].icon;
     const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
     console.log(icon_path);
-
+    document.getElementById('weather_icon').src = icon_path;
+    
 } // end of onload
